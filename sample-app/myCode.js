2d07c93103ced38979b86b50d75b8e6b39df4bb0
Hovav Oppenheim <hovav@bnop.co>
2017-12-03 07:36:11 +0200

    Cleanup

--- sample-app/myCode.js
+++ sample-app/myCode.js
@@ -2,13 +2,10 @@
 project.svgName = 'design.svg';
 
 project.setup = function () {
-//    layer('cat').pivot = layer('cat').bounds.center * 2;
-//    layer('cat').selected = true;
-//    console.log(layer('cat').position);
+
 }
 
 project.step = function () {
     layer('cat').rotation = yoyo(180, data);
-//    console.log(layer('cat').position);
     layer('text').position.y = data * 200 + 400;
 }
