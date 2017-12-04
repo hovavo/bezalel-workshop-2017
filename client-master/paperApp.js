f4ee9dea59a95815ae0108517e311fedfbb9e01b
Hovav Oppenheim <hovav@bnop.co>
2017-12-04 22:22:12 +0200

    Added Leap Motion input

--- client-master/paperApp.js
+++ client-master/paperApp.js
@@ -4,15 +4,23 @@ project.currentStyle.fillColor = 'white';
 
 var track = new Path([view.bounds.leftCenter, view.bounds.rightCenter]);
 var el = new Shape.Circle(view.center, 50);
-var value = 0.5;
+// var value = 0.5;
 
-function onMouseMove(event) {
-  value = event.point.x / view.size.width;
-  socket.emit('data', value);
-  el.position.x = event.point.x;
-}
+// function onMouseMove(event) {
+//   value = event.point.x / view.size.width;
+//   socket.emit('data', value);
+//   el.position.x = event.point.x;
+// }
 
 function onResize(event) {
   track.segments = [view.bounds.leftCenter, view.bounds.rightCenter];
   el.position.y = view.center.y;
 }
+
+Leap.loop(function(frame){
+  if (frame.hands.length) {
+    var val = frame.hands[0].palmPosition[1] / 400;
+    el.position.x = val * view.size.width;
+    socket.emit('data', val);
+  }
+});
